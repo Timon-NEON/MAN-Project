73dce245b800a1162dc21db108d7d9ed47536720
Timon-NEON <tymofeykv@gmail.com>
2023-07-03 18:14:18 +0300

    add permutation algoritm

---
+++ Helpfull_algoritms/permutation_algoritm.py
@@ -0,0 +1,38 @@
+def create_permutation (array:list, length:int, required_length:int, alowed_values:list):
+    if length == required_length:
+        for number in alowed_values:
+            array.append(number)
+            main_array.append(array.copy())
+            array.pop(len(array) - 1)
+    else:
+        length += 1
+        for number in alowed_values.copy():
+            array.append(number)
+            alowed_values.remove(number)
+            create_permutation(array, length, required_length, alowed_values)
+            array.pop(len(array) - 1)
+            alowed_values.insert(0, number)
+
+
+
+def start_permutation (required_length:int):
+    array = []
+    length = 1
+    alowed_values = []
+    for number in range(1, required_length + 1):
+        alowed_values.append(number)
+    create_permutation(array, length, required_length, alowed_values)
+
+
+required_length = 8
+
+main_array = []
+start_permutation(required_length)
+
+#for array in main_array:
+#    if main_array.count(array) > 1:
+#        print("!!!!!!!!!", array)
+#
+#    print(array)
+
+print(len(main_array))
