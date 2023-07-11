61c0139f5d89f45d7f64995045c9706076400594
Timon-NEON <tymofeykv@gmail.com>
2023-07-11 11:13:09 +0300

    update test file

--- Helpfull_algoritms/tetsing.py
+++ Helpfull_algoritms/tetsing.py
@@ -1,21 +1,13 @@
 import unittest
 from adding_word_algoritm import Crossword
 
-class Testing ():
-
-    def test_examining_word (word:str, crossword:Crossword):
-        """word check correctness test"""
-
-        new_crossword = crossword.examine_word(word, crossword)
-        print(new_crossword)
-
 
 crossword = Crossword()
-crossword.crossword = crossword.add_word("серветка", False, 0, 3, crossword.crossword)
-crossword.crossword = crossword.add_word("словник", True, 0, 3, crossword.crossword)
-crossword.crossword = crossword.add_word("букет", True, 5, 7, crossword.crossword)
+crossword.add_words(["дума", "дім", "мул", "літо", "душа"])
 
-print(crossword.crossword)
+print()
+print (crossword.all_crossword)
+for array in crossword.all_crossword[1]:
+    print(str(array))
 
-crossword.examine_word("воля", crossword.crossword)
-crossword.print_all_variants()
+crossword.create_visualisation()
