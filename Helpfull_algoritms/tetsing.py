c7f219d7e61711ebeac8d3d3ca835666433d69a0
Timon-NEON <tymofeykv@gmail.com>
2023-07-12 18:58:21 +0300

    added timer for measuerment time that required to create crosswords

--- Helpfull_algoritms/tetsing.py
+++ Helpfull_algoritms/tetsing.py
@@ -1,13 +1,19 @@
 import unittest
+import time
 from adding_word_algoritm import Crossword
 
+start_time = time.time()
 
 crossword = Crossword()
-crossword.add_words(["дума", "дім", "мул", "літо", "душа"])
+crossword.add_words(["молоко", "ручка", "ластик", "чашка", "игла"])
 
 print()
-print (crossword.all_crossword)
-for array in crossword.all_crossword[1]:
-    print(str(array))
+#print (crossword.all_crossword)
+#for array in crossword.all_crossword[1]:
+#    print(str(array))
+
+end_time = time.time()
+
+print(round(end_time - start_time, 2))
 
 crossword.create_visualisation()
