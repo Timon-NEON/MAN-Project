8afe34fb3bb257755a433145a1c16fbd7c4a6b97
Timon-NEON <tymofeykv@gmail.com>
2023-07-14 17:37:40 +0300

    comented all class

--- Helpfull_algoritms/adding_word_algoritm.py
+++ Helpfull_algoritms/adding_word_algoritm.py
@@ -8,57 +8,59 @@ import random
 
 class Crossword:
     """Generate crossword puzzles"""
-
-    def __init__(self):
-        self.shown_index = 0
+    #structure of list of crossoword: [ number of intersections (int type), [ intersections of crossword (tuple type) ], { coordinate (tupple type) : letter (string type) } ]
 
     def generate_all_crosswords (self, given_words:list):
-        self.all_crossword = [0, []]
-        length = 2
-        alowed_values = given_words
-        self.required_length = len(given_words)
-        for word in alowed_values.copy():
-            array = Crossword.__insert_word(self, word, False, 0, 0, [0, [], {}])
+        """Main public function that make preparing and start generate all variant of crosswords"""
+
+        self.all_crosswords = [0, []] #main list that keeps 2 values: 1st (int value) shows maximum naumber of intersections, 2nd (list value) keeps all crosswords that have intersections value that is equal to 1st value of self.all_crosswords
+        self.required_length = len(given_words)  #preparing of the parametr of required length of crossword for permutation function
+        length = 2 #preparing of length parametr of crossword for permutation function
+        alowed_values = given_words #list that keeps words that should be added to crossword
+        for word in alowed_values.copy(): #start permutation
+            array = Crossword.__insert_word(self, word, False, 0, 0, [0, [], {}]) #add first word to crossword
             alowed_values.remove(word)
             Crossword.__make_permutation_all_crosswords(self, length, array, alowed_values)
             alowed_values.append(word)
 
-
     def __make_permutation_all_crosswords (self, length:int, crossword:list, alowed_values:list):
+        """Recursive private function that generate all variants of crosswords"""
 
-        if length == self.required_length:
+        if length == self.required_length: #checking that the crossword is almost done
             word = alowed_values[0]
-            exam_results = Crossword.__examine_word(self, word, copy.deepcopy(crossword))
-            if bool(exam_results):
+            exam_results = Crossword.__examine_word(self, word, copy.deepcopy(crossword)) #checking that word can be added to crossword
+            if bool(exam_results): #checking that list keeps values (it can be empty)
                 for result in exam_results:
-                    if result[0] == self.all_crossword[0]:
-                        self.all_crossword[1].append(result.copy())
-                    elif result[0] > self.all_crossword[0]:
-                        self.all_crossword[0] = result[0]
-                        self.all_crossword[1].clear()
-                        self.all_crossword[1].append(result.copy())
+                    if result[0] == self.all_crosswords[0]: #checking that crossword have the same intersections value like the values of intersections from self.all_crosswords
+                        self.all_crosswords[1].append(result.copy()) #adding new crossword to the self.all_crosswords
+                    elif result[0] > self.all_crosswords[0]: #checking that crossword have the bigger intersections value than the value of intersections from self.all_crosswords
+                        self.all_crosswords[0] = result[0] #updating the values of intersection self.all_crosswords
+                        self.all_crosswords[1].clear() #deleting all variants of crosswords in self.all_crosswords (they have the less value of intersections)
+                        self.all_crosswords[1].append(result.copy()) #adding new crossword to the self.all_crosswords
         else:
             length += 1
-            for word in alowed_values.copy():
+            for word in alowed_values.copy(): #adding to crossword words from alowed_values
                 alowed_values.remove(word)
-                exam_results = Crossword.__examine_word(self, word, copy.deepcopy(crossword))
-                if bool(exam_results):
+                exam_results = Crossword.__examine_word(self, word, copy.deepcopy(crossword)) #checking that word can be added to crossword
+                if bool(exam_results): #checking that list keeps values (it can be empty)
                     for result in exam_results:
-                        Crossword.__make_permutation_all_crosswords(self, length, result, alowed_values)
+                        Crossword.__make_permutation_all_crosswords(self, length, result, alowed_values) #making recursive to adding all words to crossword
                 alowed_values.append(word)
 
     def generate_one_crossword(self, given_words:list, required_intersections):
-        self.all_crossword = [0, []]
-        self.required_intersections = required_intersections
-        self.required_length = len(given_words)
-        self.to_calculate = True
-        length = 2
-        alowed_values = []
-        for temp in range(self.required_length):
+        """Main public function that make preparing and start generate for one variant of crossword"""
+
+        self.all_crosswords = [0, []] #main list that keeps 2 values: 1st (int value) shows maximum naumber of intersections, 2nd (list value) keeps all crosswords that have intersections value that is equal to 1st value of self.all_crosswords
+        self.required_intersections = required_intersections #preparing of necessary parametr for permutation function
+        self.required_length = len(given_words) #preparing of the parametr of required length of crossword for permutation function
+        self.to_calculate = True #preparing of boolean parametr for permutation function which determines whether the program still need to look for acceptable variant of crossword
+        length = 2 #preparing of length parametr of crossword for permutation function
+        alowed_values = [] #list that keeps words that should be added to crossword
+        for temp in range(self.required_length): #generating random word order for alowed_values (for making different crosswords from the same input words)
             new_value = random.choice(given_words)
             alowed_values.append(new_value)
             given_words.remove(new_value)
-        for word in alowed_values.copy():
+        for word in alowed_values.copy(): #start permutation
             if self.to_calculate:
                 array = Crossword.__insert_word(self, word, False, 0, 0, [0, [], {}])
                 alowed_values.remove(word)
@@ -66,48 +68,53 @@ class Crossword:
                 alowed_values.append(word)
 
     def __make_permutation_one_crosswords (self, length:int, crossword:list, alowed_values:list):
-        if length == self.required_length and self.to_calculate:
+        """Recursive private function that generate one variant of crossword"""
+
+        if length == self.required_length and self.to_calculate: #checking that the crossword is almost done and whether the program still need to look for acceptable variant of crossword
             word = alowed_values[0]
-            exam_results = Crossword.__examine_word(self, word, copy.deepcopy(crossword))
-            if bool(exam_results):
+            exam_results = Crossword.__examine_word(self, word, copy.deepcopy(crossword)) #checking that word can be added to crossword
+            if bool(exam_results): #checking that list keeps values (it can be empty)
                 for result in exam_results:
                     if result[0] > self.required_intersections - 1 and self.to_calculate:
-                        self.all_crossword[0] = result[0]
-                        self.all_crossword[1].append(result.copy())
-                        self.to_calculate = False
-        elif self.to_calculate:
+                        self.all_crosswords[0] = result[0]
+                        self.all_crosswords[1].append(result.copy())
+                        self.to_calculate = False #change search status
+        elif self.to_calculate: #checking whether the program still need to look for acceptable variant of crossword
             length += 1
-            for word in alowed_values.copy():
+            for word in alowed_values.copy(): #adding to crossword words from alowed_values
                 if self.to_calculate:
                     alowed_values.remove(word)
                     exam_results = Crossword.__examine_word(self, word, copy.deepcopy(crossword))
-                    if bool(exam_results):
+                    if bool(exam_results): #checking that list keeps values (it can be empty)
                         for result in exam_results:
-                            Crossword.__make_permutation_one_crosswords(self, length, result, alowed_values)
+                            Crossword.__make_permutation_one_crosswords(self, length, result, alowed_values) #making recursive to adding all words to crossword
                     alowed_values.append(word)
 
     def __examine_word (self, word:str, crossword:list):
-        possible_intersections = {}
+        """Auxiliary private function that cheks whether a word can be added to a given crossword.
+        If it`s true, function return list with all variants of crosswords with a new word.
+        Else funtion return empty list"""
 
-        new_crosswords = []
+        possible_intersections = {} #dictionary with key (tuple type) is coordinate of letter and values (string type) is letter, that coincides with letter in crossword
+        new_crosswords = [] #list that keeps all variants of crosswords with a new word
 
-        for coordinate, letter in crossword[2].items():                       #EXAM 0
+        for coordinate, letter in crossword[2].items(): #preparing possible intersections
             if letter in word:
                 possible_intersections[coordinate] = letter
 
-        for coordinate in possible_intersections.copy():      #EXAM 1
+        for coordinate in possible_intersections.copy(): #CHECKING 1: removing from possible intersections a intersrction if this intersection have crossword (second value keeps all crossword`s intersections)
             if coordinate in crossword[1]:
                 possible_intersections.pop(coordinate)
 
         for coordinate, letter in possible_intersections.items():
             index = -1
-            if ((coordinate[0] - 1, coordinate[1]) in crossword[2].keys()    #EXAM 2
+            if ((coordinate[0] - 1, coordinate[1]) in crossword[2].keys() #CHECKING 2: definition of the derection vector of word
                 or (coordinate[0] + 1, coordinate[1]) in crossword[2].keys()):
                 direction_vector = True # 1 vertical vector; 0 horizontal vector
             else:
                 direction_vector = False
 
-            for count in range(word.count(letter)):                      #EXAM3
+            for count in range(word.count(letter)): #CHECKING 3: checking for empty cells next to those cells in which the program wants to add a word
                 index = word.index(letter, index + 1)
                 approve = True
                 intersections = []
@@ -176,45 +183,46 @@ class Crossword:
                         new_crosswords.append(temp_crossword.copy())
                     else:
                         continue
-        return new_crosswords
-
+        return new_crosswords #returning answer of function
 
     def __insert_word (self, word : str, vector:bool, first_x : int, first_y: int, new_crossword:list):
-        x = first_x
-        y = first_y
-        if vector == False:
-            add_x = 1
-            add_y = 0
-        else:
+        """Auxiliary private function that insert a word into given crossword by given parametrs and return it"""
+
+        x = first_x #start x coordinate
+        y = first_y #start y coordinate
+        if vector: #checking vector and giving instructions
             add_x = 0
             add_y = -1
-        for letter in word:
+        else:
+            add_x = 1
+            add_y = 0
+        for letter in word: #inserting a word into a given crossword
             coordinate = (x, y)
             if not(coordinate in new_crossword[2].keys()):
                 new_crossword[2][coordinate] = letter
             x += add_x
             y += add_y
-        return new_crossword
-
+        return new_crossword #returning answer of function
 
     def create_visualisation (self):
-        self.window = Tk()
-        self.window.title('Тест кросворда')
-        # root.geometry('700x300')
+        """Main public function that make visualisation of crossword"""
 
-        self.crosword_interface = Frame()
+        self.window = Tk() #main window
+        self.window.title('Crossword puzzle')
+
+        self.crosword_interface = Frame() #frame that keeps buttons and frame with crossword
         self.crosword_interface.grid(row=0, column=0)
-        self.crossword_visual = Frame(master=self.crosword_interface)
+        self.crossword_visual = Frame(master=self.crosword_interface) #frame that keeps crossword
         self.crossword_visual.grid(row=0, column=0, columnspan=2)
 
-        if len(self.all_crossword[1]) != 0:
+        if len(self.all_crosswords[1]) != 0: #checking that availability of variants of crosswords
             self.shown_index = 0
-            self.__create_crossword_structer(self.all_crossword[1][self.shown_index])
+            self.__create_crossword_structer(self.all_crosswords[1][self.shown_index]) #creating crossword structure
         else:
-            self.__message_no_crosswords()
+            self.__message_no_crosswords() #outputting message about none variants of crosswords
 
-        btn_font = font.Font(size=16)
-        if len(self.all_crossword[1]) > 1:
+        btn_font = font.Font(size=16) #creating font for buttons
+        if len(self.all_crosswords[1]) > 1: #checking that crossword have more than 1 variant for buttons
             button_forward = Button(master=self.crosword_interface, text="▶", width=3, height=1, font=btn_font,
                                     command=lambda: Crossword.__forward_visual_btncommand(self))
             button_forward.grid(row=1, column=1)
@@ -225,9 +233,9 @@ class Crossword:
         self.window.mainloop()
 
     def __create_crossword_structer(self, crossword_displayed):
+        """Auxiliary private function that create crossword in frame self.crossword_visual"""
+
         max_x, min_x, max_y, min_y = Crossword.__get_size(self, crossword_displayed)
-        # size_x = int (max_x - min_x)
-        # size_y = int (max_y - min_y)
         for row, coordinate_y in enumerate(range(int(max_y), int(min_y) - 1, -1)):
             for column, coordinate_x in enumerate(range(int(min_x), int(max_x) + 1)):
                 coordinate = (coordinate_x, coordinate_y)
@@ -239,30 +247,8 @@ class Crossword:
                 new_entry.grid(row=row, column=column)
                 new_entry.insert(0, text)
 
-    def __message_no_crosswords (self):
-        message_label = Label(master=self.crossword_visual, text="Нажаль з наданих слів неможливо створити кросворди.\nСпробуйте інші слова")
-        message_label.grid(row=0, column=0)
-
-    def __clear_frame_crossword_visual(self):
-        if self.crossword_visual.children:
-            for widget in self.crossword_visual.winfo_children():
-                widget.destroy()
-
-    def __forward_visual_btncommand(self):
-        if len(self.all_crossword[1]) - 1 > self.shown_index:
-            self.shown_index += 1
-            self.__clear_frame_crossword_visual()
-            self.__create_crossword_structer(self.all_crossword[1][self.shown_index])
-
-    def __backward_visual_btncommand(self):
-        if self.shown_index > 0:
-            self.shown_index -= 1
-            self.__clear_frame_crossword_visual()
-            self.__create_crossword_structer(self.all_crossword[1][self.shown_index])
-
-
     def __get_size (self, crossword:list):
-        """get max_x, min_x, max_y, min_y coordinate from crossword"""
+        """Auxiliary private function that give max_x, min_x, max_y, min_y coordinate from crossword"""
 
         min_x, min_y = float('inf'), float('inf')
         max_x, max_y = float('-inf'), float('-inf')
@@ -278,15 +264,31 @@ class Crossword:
 
         return max_x, min_x, max_y, min_y
 
+    def __message_no_crosswords (self):
+        """Auxiliary private function that output message about none variants of crosswords in frame self.crossword_visual"""
 
+        message_label = Label(master=self.crossword_visual, text="Нажаль з наданих слів неможливо створити кросворди.\nСпробуйте інші слова")
+        message_label.grid(row=0, column=0)
 
-#crossword_1 = Crossword()
+    def __forward_visual_btncommand(self):
+        """Auxiliary private function that create after pressed button_forward next crossword if it exist"""
 
-#words = ["серветка", "словник", "букет"]
+        if len(self.all_crosswords[1]) - 1 > self.shown_index:
+            self.shown_index += 1
+            self.__clear_frame_crossword_visual()
+            self.__create_crossword_structer(self.all_crosswords[1][self.shown_index])
 
-#crossword_1.add_words(words)
+    def __backward_visual_btncommand(self):
+        """Auxiliary private function that create after pressed button_backward previos crossword if it exist"""
 
-#crossword_1.create_visualisation()
+        if self.shown_index > 0:
+            self.shown_index -= 1
+            self.__clear_frame_crossword_visual()
+            self.__create_crossword_structer(self.all_crosswords[1][self.shown_index])
 
+    def __clear_frame_crossword_visual(self):
+        """Auxiliary private function that clearing frame self.crossword_visual before creating new crossword"""
 
-
+        if self.crossword_visual.children:
+            for widget in self.crossword_visual.winfo_children():
+                widget.destroy()
