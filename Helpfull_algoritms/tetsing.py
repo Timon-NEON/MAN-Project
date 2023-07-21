084239c0aaa2f959b02bbecf390876f3d4b9e12f
Timon-NEON <tymofeykv@gmail.com>
2023-07-21 18:55:58 +0300

    added crossword-making alghoritm and launch file

--- Helpfull_algoritms/tetsing.py
+++ Helpfull_algoritms/tetsing.py
@@ -5,9 +5,11 @@ from adding_word_algoritm import Crossword
 start_time = time.time()
 
 crossword = Crossword()
-crossword.generate_one_crossword(["молоко", "свиня", "яблоко"], 2)
+crossword.generate_one_crossword(["нафта", "метан", "жири", "вуглеводи", "алкани",  "спирти",
+                                  "кислоти", "естери", "білки", "карбон", "оксиген",
+                                  "гідроген"], 11)
 
-crossword.generate_all_crosswords(["молоко", "свиня", "яблоко"])
+#crossword.generate_all_crosswords(["молоко", "свиня", "яблоко"])
 
 end_time = time.time()
 
