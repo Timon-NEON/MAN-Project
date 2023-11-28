ae7ca6bcc02c824b3c3906fec64c1b8edfe5839d
Кириленко Тимофій <tymofeykv@gmail.com>
2023-11-28 14:45:44 +0200

    update for host

--- crossword_app/settings.py
+++ crossword_app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0so#6u0r(b*#7*s0si^ey(12a230f4h$)e@)^n@rcvezxanih-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['crosswordsua.pythonanywhere.com', '127.0.0.1:8000']
 
 
 # Application definition
