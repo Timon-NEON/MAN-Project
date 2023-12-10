076eb845eab93f71af65bb8574f61552207f9012
Кириленко Тимофій <tymofeykv@gmail.com>
2023-12-10 20:13:40 +0200

    added text key file

--- crossword_app/settings.py
+++ crossword_app/settings.py
@@ -12,12 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-try:
-    from dotenv import load_dotenv
-except:
-    from python-dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+f = open ('key.txt')
+BOT_TOKEN = f.readline()
+
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
